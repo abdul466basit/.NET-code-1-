0a6315af33e8a4ec707ac13958c171ab10ac78b9
Basit <abdul@breakthru.pk>
2025-03-05 11:27:30 +0500

    Remove OrderController and Its DTO

--- obj/Debug/net8.0/EFCrud.AssemblyInfo.cs
+++ obj/Debug/net8.0/EFCrud.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCrud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ca7b822f93899c6841724ff87f4716c331a9f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+886c1db8c9fb564c0b7187931e35100d53b79ab5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCrud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCrud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
